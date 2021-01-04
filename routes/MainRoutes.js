7dde5e2fd0c37474c45356f4772c6eeae29de996
Nathan <nathan.meneguz@epitech.eu>
2021-01-04 08:13:57 +0100

    [+] - Adding routes and pushing postman collection

--- routes/MainRoutes.js
+++ routes/MainRoutes.js
@@ -1,5 +1,6 @@
 const Router = require('express').Router();
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 Router.use((req,res,next) => {
     const date = new Date();
@@ -7,25 +8,46 @@ Router.use((req,res,next) => {
     next();
 })
 
-Router.post('/login', (req,res) => {
+Router.post('/login', (req, res) => {
     const goodCredentials = {
         username: 'aled',
         password: 'workshop'
     }
-    if (!req.body || !req.body.password) {
-        res.status(400).send({status: 400, message:'error'});
+    if (!req.body || !req.body.username || !req.body.password) {
+        res.status(400).send({status: 400, message:'Bad request'});
+    } else if (req.body.username === goodCredentials.username && req.body.password === goodCredentials.password) {
+        res.status(200).send({status: 200, message:'Ok.'});
     } else {
-        res.status(200).send({status:200, message:'done'});
+        res.status(401).send({status: 400, message:'Unauthorized'});
         console.log(`[✅] -- Logged in!`)
     }
 })
 
+Router.post('/register', (req, res) => {
+    if (!req.body || !req.body.email || !req.body.username || !req.body.password)
+        res.status(400).send({status: 400, message: 'Bad request'});
+    else {
+        res.status(200).send({status: 200, message: 'OK.'});
+        console.log(`[✅] -- New user registered`);
+    }
+});
+
+Router.get('/pupper', (req, res) => {
+    const pupPath = __dirname + '/../public/img/shibe.jpg';
+    console.log(pupPath);
+    if (!fs.existsSync(pupPath))
+        res.status(404).send({status: 404, message: 'Pupper not found. :('});
+    else {
+        res.status(200).download(pupPath);
+    }
+})
+
 Router.get('/teapot', (req, res) => {
-    res.status(417).send({status: 417, message:'teapotlol'});
+    res.status(418).send({status: 418, message:'I\'m a teapot.'});
     console.log('[🫖] -- I\'m a teapot dude');
 });
 
-Router.post('/ping', (req, res) => {
+Router.get('/ping', (req, res) => {
     res.status(200).send({status:200, message:'Pong!'});
     console.log(`[✅] -- Pong lmao`);
 });
